45c52171f32e9403ee506f20c04a5659cb4e0c60
promonkeyli <liyang_email@163.com>
2025-11-26 09:18:34 +0800

    fix(frontend/mobile-taro): 修复zustand在taro中编译小程序报错的问题

--- frontend/mobile-taro/config/dev.ts
+++ frontend/mobile-taro/config/dev.ts
@@ -1,7 +1,10 @@
 import type { UserConfigExport } from "@tarojs/cli"
 
 export default {
-  
-  mini: {},
+
+  mini: {
+    // 注：添加该属性的原因详见：https://github.com/NervJS/taro/issues/17350
+    debugReact: true
+  },
   h5: {}
 } satisfies UserConfigExport<'vite'>
